7d4ecd10aa810830e7ee1d8a83c586c57028185b
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-15 15:01:39 +0530

    'catalog' app urls.py configured.

--- src/locallibrary/locallibrary/settings.py
+++ src/locallibrary/locallibrary/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Own Apps
+    'catalog.apps.CatalogConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
