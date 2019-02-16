31b41e69baecef0b478e3375970e38259e41a8cc
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-16 21:52:44 +0530

    removed environment files.

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\kirar\documents\python\dev\django\mdn\local-library\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
