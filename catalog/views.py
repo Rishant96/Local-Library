29534fff01f0a33ffebc3186602a37e0af6d439d
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-18 13:37:47 +0530

    completed 'Part 6 - Pagination'.

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Author, Book, BookInstance, Genre
+
+from django.views import generic
 
 
-def index(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello World</h1>')
+def index(request):
+    """View function for the home page of the site."""
+
+    # Generates counts of some the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects\
+        .filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template 'index.html' with the data in the
+    # context variable
+    return render(request, 'catalog/index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
